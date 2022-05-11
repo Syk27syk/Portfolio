96d4cad216f8719e98595ffedbbc201c475f5869
shiyunkhoo <shiyun.khoo@gmail.com>
2022-05-11 22:24:55 +0800

    hash

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,67 @@
 <template>
-  <div class="flex h-screen bg-slate-600 h-[100vh] align-middle text-white text-center">
+  <div class="flex h-screen bg-slate-600 align-middle text-white text-center">
     <div class="m-auto">
       <h1 class="text-5xl">Portfolio</h1>
       <p class="mt-5">testing</p>
+      <div id="search">
+        <p>search by: technology/skill</p>
+        <p>search by: project criteria and type</p>
+        <p>search by: industry/domain</p>
+        <p>sort by: date</p>
+      </div>
+      <div id="recent">
+        recent
+      </div>
+      <div id="featured">
+        featured
+      </div>
+      <div id="web development">
+        <div id="front-end">
+          <p> front ends and web design </p>
+          <p> styles e.g. minimalist, neumorphic </p>
+          <p> animated, webGL </p>
+          <p> showcase variety and understanding of common requirements and your ability to hit them</p>
+          <p> add explanation of your processes and about the project </p>
+        </div>
+        <div id="audio-programming">
+          audio programming: synthesizers, audiovisualizers, spectrograms, audio analyzers, generative music, signal processing, fluid dynamics
+        </div>
+        <div id="web-gl">
+          webGL projects
+        </div>
+        <div id="creative-coding-and-generative-art">
+          creative coding
+        </div>
+        <div id="media">
+          web audio, web media ...
+        </div>
+        <div id="personal-projects">
+          <p>personal and class projects, e.g. standard curriculum projects + personal interests</p>
+          <p> matterJS, toneJS </p>
+        </div>
+      </div>
+      <div id="data-science">
+        <p>data science projects: visualizations, scraping, machine learning</p>
+        <p>classification by industry and field, methods</p>
+      </div>
+      <div id="IoT-projects">
+        internet of things/physical computing
+      </div>
+      <div id="other">
+        Projects with: go, dart, swift, flutter, firebase, AWS, Azure, PHP, Ruby on Rails, C, C++, Python
+      </div>
+      <div id="infographics-videos-courses">
+        infographics, videos, courses, teaching
+      </div>
+      <div id="qualifications-and-certifications">
+        qualifications and certifications and events attended
+      </div>
+      <div id="soft skills">
+        something to demonstrate soft skills
+      </div>
+      <div id="references">
+        references
+      </div>
     </div>
   </div>
 </template>
